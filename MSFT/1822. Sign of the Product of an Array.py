f740561903d4d8020e1aec320d43890cece2ef0d
Allan Kiche <tle2819@students.mu.ac.ke>
2023-09-25 06:12:54 +0300

    1822

--- MSFT/1822. Sign of the Product of an Array.py
+++ MSFT/1822. Sign of the Product of an Array.py
@@ -6,3 +6,12 @@
 # You are given an integer array nums. Let product be the product of all values in the array nums.
 
 # Return signFunc(product).
+
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        neg = 0
+        for n in nums:
+            if n == 0:
+                return 0
+            neg += (1 if n < 0 else 0)
+        return -1 if neg % 2 else 1
