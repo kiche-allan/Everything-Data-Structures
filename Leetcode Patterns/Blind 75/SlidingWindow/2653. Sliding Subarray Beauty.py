0f4ad93d06178f1ed539f4478624ef7620774c3a
Allan Kiche <tle2819@students.mu.ac.ke>
2023-10-05 09:58:45 +0300

    sol

--- Leetcode Patterns/Blind 75/SlidingWindow/2653. Sliding Subarray Beauty.py
+++ Leetcode Patterns/Blind 75/SlidingWindow/2653. Sliding Subarray Beauty.py
@@ -5,3 +5,26 @@
 # Return an integer array containing n - k + 1 integers, which denote the beauty of the subarrays in order from the first index in the array.
 
 # A subarray is a contiguous non-empty sequence of elements within an array.
+from sortedcontainers import SortedList
+class Solution:
+    def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
+        ans = []
+
+        N = len(nums)
+        sorted_list = SortedList()
+        for i in range(N):
+            sorted_list.add(nums[i])
+
+            if i - k >= 0:
+                sorted_list.remove(nums[i-k])
+
+            if len(sorted_list) >= k:
+                #print the sorted list
+                t = sorted_list[x -1]
+                if t >= 0:
+                    ans.append(0)
+                else:
+                    ans.append(t)
+        return ans
+                    
+
