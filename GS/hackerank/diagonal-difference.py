290a13b8f1fe4f175e22dc5d43e912a552414111
Allan Kiche <tle2819@students.mu.ac.ke>
2023-10-26 15:38:00 +0300

    hackerank

---
+++ GS/hackerank/diagonal-difference.py
@@ -0,0 +1,8 @@
+# Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+# For example, the square matrix  is shown below:
+
+# 1 2 3
+# 4 5 6
+# 9 8 9  
+# The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
