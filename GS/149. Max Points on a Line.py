3fe54995e85e16bb3766bb477a44facc06f38af4
Allan Kiche <tle2819@students.mu.ac.ke>
2023-10-26 05:11:21 +0300

    max points on a line

--- GS/149. Max Points on a Line.py
+++ GS/149. Max Points on a Line.py
@@ -1,1 +1,17 @@
 # Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        res = 1
+        for i in range(len(points)):
+            p1 = points[i]
+            count = collections.defaultdict(int)
+            for j in range(i + 1, len(points)):
+                p2 = points[j]
+                if p2[0] == p1[0]:
+                    slope = float("inf")
+                else:
+                    slope = (p2[1] -p1[1]) / (p2[0] - p1[0])
+                count[slope] += 1
+                res = max(res, count[slope] +1)
+        return res
