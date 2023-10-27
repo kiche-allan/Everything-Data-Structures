40dfdaccc974cb5240295cf92364acf5f3ad6ba9
Allan Kiche <tle2819@students.mu.ac.ke>
2023-10-27 11:40:53 +0300

    reducedstring

---
+++ GS/hackerank/ReducedStrings.py
@@ -0,0 +1,11 @@
+# Reduce a string of lowercase characters in range ascii[‘a’..’z’]by doing a series of operations. In each operation, select a pair of adjacent letters that match, and delete them.
+
+# Delete as many characters as possible using this method and return the resulting string. If the final string is empty, return Empty String
+
+# Example.
+
+
+# aab shortens to b in one operation: remove the adjacent a characters.
+
+
+# Remove the two 'b' characters leaving 'aa'. Remove the two 'a' characters to leave ''. Return 'Empty String'.
