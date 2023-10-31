812e1bde2feb81e1cdcd2c332d2a700b0f0f6104
Allan Kiche <tle2819@students.mu.ac.ke>
2023-10-31 13:49:53 +0300

    2PY

--- Leetcode/LinkedList/2.py
+++ Leetcode/LinkedList/2.py
@@ -3,30 +3,34 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        
         dummy = ListNode()
         cur = dummy
-        
         carry = 0
-        while l1 or 12 or carry:
+
+        while l1 or l2 or carry:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
-            
-            #newdigit
+
+            #new digit
             val = v1 + v2 + carry
             carry = val // 10
             val = val % 10
             cur.next = ListNode(val)
-            
-            #update pt
+
+            #update pointers 
             cur = cur.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
-            
         return dummy.next
-    
+
+
     
 #     Complexity Analysis
 
