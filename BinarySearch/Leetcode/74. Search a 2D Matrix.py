9770f9c5884f176a63f6d62ea580bab1100a9231
Allan Kiche <tle2819@students.mu.ac.ke>
2023-11-08 21:17:30 +0300

    sol

--- BinarySearch/Leetcode/74. Search a 2D Matrix.py
+++ BinarySearch/Leetcode/74. Search a 2D Matrix.py
@@ -6,4 +6,21 @@
 
 # You must write a solution in O(log(m * n)) time complexity.
 
- 
+ class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        if not (top <= bot):
+            return False
+        row = (top + bot) // 2
+        l , r = 0, COLS - 1
+        return target in matrix[row]
