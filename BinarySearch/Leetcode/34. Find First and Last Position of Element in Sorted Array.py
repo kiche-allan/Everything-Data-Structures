4a00a1e5f9ecd6263f391ef65cfe863a262fb54f
Allan Kiche <tle2819@students.mu.ac.ke>
2023-11-08 14:17:21 +0300

    74

--- BinarySearch/Leetcode/34. Find First and Last Position of Element in Sorted Array.py
+++ BinarySearch/Leetcode/34. Find First and Last Position of Element in Sorted Array.py
@@ -3,3 +3,36 @@
 # If target is not found in the array, return [-1, -1].
 
 # You must write an algorithm with O(log n) runtime complexity.
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if not nums:
+            return [-1, -1]
+        l, r = 0, len(nums) - 1
+
+        #find the first occurence of the target value
+
+        while l <= r:
+            m = (l + r ) // 2
+            if nums[m] == target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+            if first == -1:
+                return [-1, -1]
+            # Find the last occurrence of the target value
+            last = first
+            while l <= r:
+                m = (l + r ) // 2
+                if nums[m] == target:
+                    last= m
+                    l = m + 1
+                else:
+                    r = m - 1
+            return [first, last]
+        
+
+    
