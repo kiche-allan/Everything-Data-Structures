8700852d86bbb24cd42f74f841e421d1530f33ac
Allan Kiche <tle2819@students.mu.ac.ke>
2023-11-10 22:20:01 +0300

    367

--- BinarySearch/Leetcode/367. Valid Perfect Square.py
+++ BinarySearch/Leetcode/367. Valid Perfect Square.py
@@ -4,3 +4,38 @@
 
 # You must not use any built-in library function, such as sqrt.
 
+# class Solution:
+#     def isperfectSquare(self, num: int) -> bool:
+#         #0(sqet(num))
+#         # for i in (1, num +1):
+#         #     if i * i == num:
+#         #         return True
+#         # 0(log n)
+#         l = 1
+#         r = num
+
+#         while l <= r:
+#             mid = (l+r) // 2
+#             if mid * mid == num:
+#                 r = mid - 1
+#             elif mid * mid < num:
+#                 l = mid + 1
+#             else:
+#                 return True
+#         return False
+
+
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        l = 1
+        r = num
+
+        while l <= r:
+            mid = (l + r) // 2
+            if mid * mid >  num:
+                r = mid - 1
+            elif mid * mid < num:
+                l = mid + 1
+            else:
+                return True
+        return False
