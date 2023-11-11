89ac89a1653f5b8679da71789f06028efae63b46
Allan Kiche <tle2819@students.mu.ac.ke>
2023-11-11 12:12:20 +0300

    222

--- BinarySearch/Leetcode/222. Count Complete Tree Nodes.py
+++ BinarySearch/Leetcode/222. Count Complete Tree Nodes.py
@@ -4,3 +4,8 @@
 
 # Design an algorithm that runs in less than O(n) time complexity.
 
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
