8fa6b471019e4f074e4467ed4613b639191518aa
Allan Kiche <tle2819@students.mu.ac.ke>
2023-11-12 21:54:27 +0300

    sol

--- Leetcode Patterns/Blind 75/121. Best Time to Buy and Sell Stock.py
+++ Leetcode Patterns/Blind 75/121. Best Time to Buy and Sell Stock.py
@@ -3,3 +3,19 @@
 # You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+class Solution:
+    def BuyandSellStocks(self, prices: List[int]) -> int:
+        l = 0
+        r = 1
+        maxProfit = 0
+
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                profit = prices[r] - prices[l]
+                maxProfit = max(maxProfit, profit)
+            else:
+                l = r
+            r += 1
+        return maxProfit
+
+
