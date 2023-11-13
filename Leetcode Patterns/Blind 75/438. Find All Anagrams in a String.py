bb01ea29211c7ff2e5a86ed70acce25ab6f2cc7c
Allan Kiche <tle2819@students.mu.ac.ke>
2023-11-13 11:25:19 +0300

    anagrams

--- Leetcode Patterns/Blind 75/438. Find All Anagrams in a String.py
+++ Leetcode Patterns/Blind 75/438. Find All Anagrams in a String.py
@@ -1,3 +1,28 @@
 # Given two strings s and p, return an array of all the start indices of p's anagrams in s. You may return the answer in any order.
 
 # An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(p) > len(s):
+            return []
+        pCount, sCount = {}, {}
+
+        for i in range(len(p)):
+            pCount[p[i]] = 1 + pCount.get(p[i], 0)
+            sCount[s[i]] = 1 + sCount.get(s[i], 0)
+
+        res = [0] if sCount == pCount else []
+        l = 0
+
+            
+        for r in range(len(p), len(s)):
+            sCount[s[r]] = 1 + sCount.get(s[r], 0)
+            sCount[s[l]] -= 1
+
+            if sCount[s[l]] == 0:
+                sCount.pop(s[l])
+            l += 1
+
+            if sCount == pCount:
+                res.append(l)
+        return res
