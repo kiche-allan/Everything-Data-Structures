afd81bf50c400dd49df189fc65c381a6dda0cbfb
Allan Kiche <tle2819@students.mu.ac.ke>
2023-11-24 21:32:54 +0300

    253

--- MSFT/1304. Find N Unique Integers Sum up to Zero.py
+++ MSFT/1304. Find N Unique Integers Sum up to Zero.py
@@ -1,1 +1,11 @@
 # Given an integer n, return any array containing n unique integers such that they add up to 0.
+class Solution:
+    def sumZero(self, n:int) -> List[int]:
+        res = []
+        if n % 2 != 0:
+            res.append(0)
+        for i in range(1, n// 2 + 1):
+            res.append(i)
+            res.append(-i)
+        return res
+         
