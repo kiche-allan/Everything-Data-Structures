479b1a47beea841d32eae61ffced89db224a555f
Allan Kiche <tle2819@students.mu.ac.ke>
2023-11-24 23:03:29 +0300

    253

--- MSFT/253. Meeting Rooms II.py
+++ MSFT/253. Meeting Rooms II.py
@@ -1,1 +1,26 @@
 # Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: the minimum number of conference rooms required
+    """
+    def min_meeting_rooms(self, intervals: List[Interval]) -> int:
+        # Write your code here
+        start = sorted([i.start for i in intervals])
+        end = sorted([i.end for i in intervals])
+
+        res, count = 0, 0
+        s, e = 0, 0
+
+        while s < len(intervals):
+            if start[s] < end[e]:
+               s += 1
+               count += 1
+
+
+            else:
+                e += 1
+                count -= 1
+            res = max(res, count)
+        return res
