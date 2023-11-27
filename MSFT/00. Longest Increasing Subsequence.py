c7d6269ccd0a0d8fe6702b4a0d2e458389ce894c
Allan Kiche <tle2819@students.mu.ac.ke>
2023-11-27 09:15:47 +0300

    300

--- MSFT/00. Longest Increasing Subsequence.py
+++ MSFT/00. Longest Increasing Subsequence.py
@@ -2,3 +2,15 @@
 # subsequence
 # .
 
+class Solution:
+    def lengthofLIS(self, nums):
+        if not nums:
+            return 0
+        n = len(nums)
+        dp = [1]* n
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp = max(dp[i], dp[j]+1)
+        return max(dp)
