057b04a063bc856a6be4c826ed7490d68e975eb0
Allan Kiche <tle2819@students.mu.ac.ke>
2023-12-01 21:56:24 +0300

    143

--- Stacks/143. Reorder List.py
+++ Stacks/143. Reorder List.py
@@ -5,4 +5,41 @@
 
 # L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
 # You may not modify the values in the list's nodes. Only nodes themselves may be changed.
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        #step 1: find the middle
+        if not head:
+            return []
+        slow, fast = head, head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        #step 2 revverse the second half
+        prev, curr = None, slow.next
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        slow.next = None
+
+        #step 3 : merge lists
+        head1, head2 = head, prev
+        while head2:
+            next = head1.next
+            head1.next = head2
+            head1 = head2
+            head2 = next
+
+        
 
