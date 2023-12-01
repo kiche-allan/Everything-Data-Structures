4e00687928bc1d83ae3ebbf5bd9e1d4bd7b1ec32
Allan Kiche <tle2819@students.mu.ac.ke>
2023-12-01 21:25:11 +0300

    143

--- Stacks/739. Daily Temperatures.py
+++ Stacks/739. Daily Temperatures.py
@@ -7,3 +7,14 @@
 # Input: temperatures = [73,74,75,71,69,72,76,73]
 # Output: [1,1,4,2,1,1,0,0]
 
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0] * len(temperatures)
+
+        for i, t in enumerate(temperatures):
+            while stack and stack[-1][0] < temp:
+                warmest_day = stack.pop()
+                res[warmest_day[1]] = i - warmest_day[1]
+            stack.append([t, i])
+        return res
