edc0423d2212d441a881cebfe9a71650f01334e3
Allan Kiche <tle2819@students.mu.ac.ke>
2023-12-02 10:05:32 +0300

    ans

--- Stacks/316. Remove Duplicate Letters.py
+++ Stacks/316. Remove Duplicate Letters.py
@@ -2,3 +2,20 @@
 # the smallest in lexicographical order
 #  among all possible results.
 
+class Solution:
+    def removeDuplicateLetters(self, s:str) -> str:
+        last_occurence = {char: i for i, char in enumerate(s)}
+        
+        visited = set()
+        stack = []
+        
+
+        for i, char in enumerate (s):
+            if char in visited:
+                continue
+            while stack and char < stack[-1] and i < last_occurence[stack[-1]]:
+                visited.remove(stack.pop())
+            
+            stack.append(char)
+            visted.add(char)
+        return ''.join(stack)
