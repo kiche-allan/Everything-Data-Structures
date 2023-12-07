1982814157ea6d9eb72f8eb3d14c9f58a433e104
Allan Kiche <tle2819@students.mu.ac.ke>
2023-12-07 22:38:19 +0300

    78

--- backtracking/78. Subsets.py
+++ backtracking/78. Subsets.py
@@ -3,3 +3,22 @@
 #  (the power set).
 
 # The solution set must not contain duplicate subsets. Return the solution in any order.
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+        
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return 
+            
+            #decision to include nums[i] in subset
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            #decisions not to include nums[i] in subset
+            subset.pop()
+            dfs(i + 1)
+        dfs(0)
+        return res
