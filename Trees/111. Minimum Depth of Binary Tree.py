f58a715345de39d097b4ab3eb7ec07f96a547ec6
Allan Kiche <tle2819@students.mu.ac.ke>
2023-12-26 21:08:00 +0300

    111

--- Trees/111. Minimum Depth of Binary Tree.py
+++ Trees/111. Minimum Depth of Binary Tree.py
@@ -4,3 +4,14 @@
 
 # Note: A leaf is a node with no children.
 
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
+        if left == 0:
+            return right + 1
+        elif right == 0:
+            return left + 1
+        return min(left, right) + 1
