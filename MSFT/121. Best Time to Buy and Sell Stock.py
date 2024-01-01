c8bbf144d846eb496bdca89c6f4409525d625127
Allan Kiche <tle2819@students.mu.ac.ke>
2024-01-01 14:24:11 +0300

    noofislands

--- MSFT/121. Best Time to Buy and Sell Stock.py
+++ MSFT/121. Best Time to Buy and Sell Stock.py
@@ -3,3 +3,17 @@
 # You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+class Solution:
+    def maxProfits(self, prices:List[int]) -> int:
+        l = 0
+        r = 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                profit = prices[l] - prices[r]
+                maxProfit = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
